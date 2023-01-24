167cf63081fe8b70f84819dcc86da1ca78d3f8ef
Dhanunjay <aradhyuladhanunjay@gmail.com>
2023-01-24 22:24:49 +0530

    primenumber

---
+++ prime.js
@@ -0,0 +1,14 @@
+function showprimes(limit){
+    for(let number=2;number<=limit;number++)
+        if (isprime(number)) console.log(number);
+}
+
+function isprime(number){
+    for(let factor=2;factor<number;factor++)
+        if(number%factor==0)
+            return false;
+        
+    
+    return true
+}
+console.log(showprimes(10));
